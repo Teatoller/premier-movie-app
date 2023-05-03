945b25b1a8e7898eebdb33d42d175e767a9bfc61
stevenennis <steven.otieno1@gmail.com>
2023-05-03 14:51:14 +0300

    ft(routing): add routing and routing layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import api from "./api/axiosConfig";
+import Layout from "./components/Layout";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -19,7 +22,14 @@ function App() {
     getMovies()
   }, [])
   
-  return <div className="App"></div>;
+  return <div className="App">
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />}></Route>
+      </Route>
+      
+    </Routes>
+  </div>;
 }
 
 export default App;
